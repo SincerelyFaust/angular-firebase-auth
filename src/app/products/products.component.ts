eee8fa7ad6cc22e824f18c56231e02f72fb05bf7
SincerelyFaust <hedesmarin@gmail.com>
2023-10-10 18:53:52 +0200

    feat(add-product): add adding products functionality

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../shared/services/ProductService';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 interface Product {
   id: number;
@@ -10,14 +16,34 @@ interface Product {
 
 type Products = Array<Product>;
 
+interface PriceValidationError {
+  invalidPrice: boolean;
+}
+
+type PriceValidatorFn = (
+  control: AbstractControl
+) => PriceValidationError | null;
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
 })
 export class ProductsComponent implements OnInit {
   products: Products = [];
+  isModalHidden = true;
+  productForm: FormGroup;
+  hasFormError = false;
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private fb: FormBuilder
+  ) {
+    this.productForm = this.fb.group({
+      productName: ['', [Validators.required, Validators.maxLength(32)]],
+      productPrice: ['', [Validators.required, this.priceValidator()]],
+      productDesc: ['', [Validators.required, Validators.maxLength(160)]],
+    });
+  }
 
   async ngOnInit(): Promise<void> {
     (await this.productService.fetchProducts()).subscribe({
@@ -31,26 +57,72 @@ export class ProductsComponent implements OnInit {
   }
 
   onAddProduct() {
-    this.productService
-      .addProduct({
-        name: 'Product 3',
-        id: 2,
-        price: 3,
-        description: 'Product 3 description',
-      })
-      .subscribe({
+    this.productForm.markAllAsTouched();
+
+    if (this.productForm.valid) {
+      const productData = {
+        name: this.productForm.get('productName')?.value,
+        price: this.productForm.get('productPrice')?.value,
+        description: this.productForm.get('productDesc')?.value,
+        id: this.highestId(),
+      };
+
+      this.productService.addProduct(productData).subscribe({
         next: v => {
-          this.products.push({
-            name: 'Product 3',
-            id: 2,
-            price: 3,
-            description: 'Product 3 description',
-          });
+          this.products.push(productData);
           console.log('Product added successfully:', v);
+          this.openModal();
         },
         error: e => {
+          this.hasFormError = true;
           console.error(e);
         },
       });
+    }
+  }
+
+  highestId(): number {
+    let highestId = 0;
+    for (const product of this.products) {
+      if (product.id > highestId) {
+        highestId = product.id;
+      }
+    }
+    return highestId + 1;
+  }
+
+  get nameControl(): AbstractControl | null {
+    return this.productForm.get('productName');
+  }
+
+  get priceControl(): AbstractControl | null {
+    return this.productForm.get('productPrice');
+  }
+
+  get descriptionControl(): AbstractControl | null {
+    return this.productForm.get('productDesc');
+  }
+
+  priceValidator(): PriceValidatorFn {
+    return (control: AbstractControl): PriceValidationError | null => {
+      const price = control.value;
+
+      if (price === null || price === undefined) {
+        return null;
+      }
+
+      const numberIsNotZero = price > 0;
+
+      if (numberIsNotZero) {
+        return null;
+      }
+
+      return { invalidPrice: true };
+    };
+  }
+
+  openModal() {
+    this.productForm.reset();
+    this.isModalHidden = !this.isModalHidden;
   }
 }
